fe34d9e40a7a43cee9628fa0be1ed31e1fc83d01
Esther Hoeun Yu <togom11@g.skku.edu>
2022-02-07 20:06:01 +0900

    Create xaas.py

---
+++ xaas.py
@@ -0,0 +1,24 @@
+https://oneclick-cloud.com/en/blog/trends-en/cloud-computing-iaas-paas-saas/
+
+> https://stackoverflow.com/
+
+
+Puppet modules are a collection of manifests and data, which can include facts, files, and templates. Modules help you organize and reuse Puppet code by enabling you to split the code into several manifests. With the exception of the main site.
+
+> Retrieve the node object.
+>> After the primary server has received the agent-provided information for this request, it asks its configured node terminus for a node object.
+By default, the primary server uses the plain node terminus, which returns a blank node object. In this case, only manifests and agent-provided information are used in compilation.
+The next most common node terminus is the exec node terminus, which requests data from an ENC. This can return classes, variables, an environment, or a combination of the three, depending on how the ENC is designed.
+You can also write a custom node terminus that retrieves classes, variables, and environments from an external system.
+
+next 
+theory
+of 
+1. catalogue
+2. module
+3. manifest
+
+vocabularies 
+
+HW: Documentation 
+  https://puppet.com/docs/puppet/5.5/modules_fundamentals.html
